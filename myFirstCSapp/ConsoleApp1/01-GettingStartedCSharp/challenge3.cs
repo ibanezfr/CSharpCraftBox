bc47e1da8a14db3398afcac43b57888627371a60
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-04 11:26:18 -0300

    working with variables

---
+++ myFirstCSapp/ConsoleApp1/01-GettingStartedCSharp/challenge3.cs
@@ -0,0 +1,9 @@
+string firstName;
+firstName = "Franco";
+Console.WriteLine(firstName);
+firstName = "Juan";
+Console.WriteLine(firstName);
+firstName = "Pedro";
+Console.WriteLine(firstName);
+firstName = "José";
+Console.WriteLine(firstName);
