f126f8735f657def6cf4b0617592e90e5c9a3bda
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-04 12:29:27 -0300

    resolves challenge 3, 4 and 5.cs

--- myFirstCSapp/ConsoleApp1/01-GettingStartedCSharp/challenge3.cs
+++ myFirstCSapp/ConsoleApp1/01-GettingStartedCSharp/challenge3.cs
@@ -1,9 +1,23 @@
-string firstName;
-firstName = "Franco";
-Console.WriteLine(firstName);
-firstName = "Juan";
-Console.WriteLine(firstName);
-firstName = "Pedro";
-Console.WriteLine(firstName);
-firstName = "José";
-Console.WriteLine(firstName);
+// string firstName;
+// firstName = "Franco";
+// Console.WriteLine(firstName);
+// firstName = "Juan";
+// Console.WriteLine(firstName);
+// firstName = "Pedro";
+// Console.WriteLine(firstName);
+// firstName = "José";
+// Console.WriteLine(firstName);
+
+/* 
+Here's what you've learned about working with variables so far:
+
+• You must assign (set) a value to a variable before you can retrieve (get) a
+    value from a variable.
+
+• You can initialize a variable by assigning a value to the variable at the
+    point of declaration. Assignment happens from right to left. 
+
+• You use a single equals character as the assignment operator. To retrieve the
+    value from the variable, you merely use the variable's name.
+    
+*/
---
+++ myFirstCSapp/ConsoleApp1/01-GettingStartedCSharp/challenge4.cs
@@ -0,0 +1,15 @@
+// var message = "¡Hola Mundo!";
+// Console.WriteLine(message);
+
+// message = 12.23m;
+
+/*
+Here's what you've learned about the var keyword so far:
+
+• The var keyword tells the compiler to infer the data type of the variable
+  based on the value it is initialized to. 
+
+• You'll likely see the var keyword as you read other people's code; however,
+  you should use the data type when possible.
+
+*/
