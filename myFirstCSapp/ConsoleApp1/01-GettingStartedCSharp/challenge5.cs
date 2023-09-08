46ef244de400eeb9a36ac898cc2e896521a7480b
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-08 13:29:53 -0300

    resolves challenge6.cs

--- myFirstCSapp/ConsoleApp1/01-GettingStartedCSharp/challenge5.cs
+++ myFirstCSapp/ConsoleApp1/01-GettingStartedCSharp/challenge5.cs
@@ -1,9 +1,9 @@
-string name = "Bob";
-int num1 = 3;
-decimal num2 = 34.4m;
+// string name = "Bob";
+// int num1 = 3;
+// decimal num2 = 34.4m;
 
-Console.Write("Hello " + name + "! ");
-Console.Write("You have " + num1 + " in your inbox. ");
-Console.WriteLine("The tempeture is " + num2 + " celsius.");
+// Console.Write("Hello " + name + "! ");
+// Console.Write("You have " + num1 + " in your inbox. ");
+// Console.WriteLine("The tempeture is " + num2 + " celsius.");
 
-Console.WriteLine(34.40m);
+// Console.WriteLine(34.40m);
