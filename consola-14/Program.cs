8b14940f46296eee5abf7364bba1ed0ff6dd3212
francoibanezweb <francoibanez.dev@gmail.com>
2024-02-07 10:41:57 -0300

    feat: reorginize some folders

---
+++ consola-14/Program.cs
@@ -0,0 +1,24 @@
+﻿// Creating a variable inside a code block
+bool flag = true;
+if (flag)
+{
+  //int value = 10;
+  //Console.WriteLine($"Inside the code block: {value}");
+}
+
+Console.WriteLine("==============");
+// Attempt to access a variable outside the code block in which it's declared
+
+//Console.WriteLine($"Outside the code block: {value}");
+
+Console.WriteLine("==============");
+// Move the variable declaration above the code block
+int value = 0;
+if (flag)
+{
+  Console.WriteLine($"Inside the code block: {value}");
+}
+value = 10;
+Console.WriteLine($"Outside the code block: {value}");
+
+
