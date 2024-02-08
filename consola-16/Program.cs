13966e9df33ee372db4eece43182e6e28b6ec927
francoibanezweb <francoibanez.dev@gmail.com>
2024-02-07 21:07:43 -0300

    feat: solve consola-16, 17 and challenge fizzBuzz

---
+++ consola-16/Program.cs
@@ -0,0 +1,24 @@
+﻿int employeeLevel = 200;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+   case 100:
+   case 200:
+      title = "Senior Associate";
+      break;
+   case 300:
+      title = "Manager";
+      break;
+   case 400:
+      title = "Senior Manger";
+      break;
+   default:
+      title = "Associate";
+      break;
+}
+
+Console.WriteLine($"{employeeName}, {title}");
+
---
+++ consola-17/Program.cs
@@ -0,0 +1,30 @@
+﻿for (int i = 0; i < 10; i ++)
+{
+   Console.WriteLine(i);    
+   if (i == 7) break;
+}
+
+Console.WriteLine("============");
+
+string[] names = {"Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+
+Console.WriteLine("============");
+// Examine the limitation of the foreach statement
+
+foreach (var name in names)
+{
+    // Can't do this:
+  // if (name == "David") name = "Sammy";
+}
+
+// Overcoming the limitation of the foreach statement using the for statement
+
+for (int i = 0; i < names.Length; i++)
+   if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) Console.WriteLine(name);
+
