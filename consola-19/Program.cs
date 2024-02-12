3b9c833546b4a1d852109a2157e2c0203a6586c7
francoibanezweb <francoibanez.dev@gmail.com>
2024-02-12 09:55:46 -0300

    feat: solve consola-19, integerInputValidation stringArrayProcessing

---
+++ consola-19/Program.cs
@@ -0,0 +1,37 @@
+﻿//    Manage user input during this challenge
+
+/*
+string? readResult;
+Console.WriteLine("Enter a string:");
+
+do
+{
+    readResult = Console.ReadLine();
+} while (readResult == null);
+*/
+
+string? readResult;
+bool validEntry = false;
+Console.WriteLine("Enter a string containing at least three characters:");
+do
+{
+    readResult = Console.ReadLine();
+    if (readResult != null)
+    {
+        if (readResult.Length >= 3)
+        {
+            validEntry = true;
+        }
+        else
+        {
+           Console.WriteLine("Your input is invalid, please try again.");
+        }
+    }
+} while (validEntry == false);
+
+
+int numericValue = 0;
+bool validNumber = false;
+
+validNumber = int.TryParse(readResult, out numericValue);
+
---
+++ projects/challenges/integerInputValidation/Program.cs
@@ -0,0 +1,22 @@
+﻿int input;
+bool validEntry = false;
+
+Console.WriteLine("Enter an integer between 5 and 10 > ");
+
+do
+{
+   input = Console.ReadLine(); 
+   if (input >= 5 && <= 10)
+   {
+       validEntry = true;
+   }
+   else
+   {
+       Console.WriteLine("Invalid input, try again.");
+   }
+
+
+} while ( valid == false);
+
+Console.WriteLine("Your input value is correct.");
+
