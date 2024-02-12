dd80a1d014fdaf3e217f668e7220d95f638abbcc
francoibanezweb <francoibanez.dev@gmail.com>
2024-02-12 14:17:22 -0300

    feat: solve stringArrayProcessing and stringInputValidation

--- projects/challenges/stringArrayProcessing/Program.cs
+++ projects/challenges/stringArrayProcessing/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices"};
+int periodLocation = 0;
+string myString = "";
+int stringsCount = myStrings.Length;
+
+for (int i = 0; i < stringsCount; i++)
+{
+   myString = myStrings[i];
+   periodLocation = myString.IndexOf(".");
+
+
+   string mySentence;
+
+   while (periodLocation != -1)
+   {
+      mySentence = myString.Remove(periodLocation);
+      myString = myString.Substring(periodLocation + 1);
+      myString = myString.TrimStart();
+
+      periodLocation = myString.IndexOf(".");
+
+      Console.WriteLine(mySentence);
+   }
+   
+   mySentence = myString.Trim();
+   Console.WriteLine(mySentence);
+}
+
+
--- projects/challenges/stringInputValidation/Program.cs
+++ projects/challenges/stringInputValidation/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? input;
+bool validEntry = false;
+
+Console.WriteLine("Enter the role name (Administrator, Manager, User) > ");
+
+do
+{
+   input = Console.ReadLine(); 
+   input = input.Trim().ToLower();
+
+   if (input == "administrator" || input == "manager" || input == "user")
+   {
+       validEntry = true;
+   }
+   else
+   {
+       Console.WriteLine("Invalid name, try again > ");
+   }
+
+} while (validEntry == false);
+
+Console.WriteLine("Valid input value");
+
